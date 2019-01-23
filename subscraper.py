6db8fd6ebe8ac70639c8df6c812a89c63290da55
Joseph Seiler <jseiler97@gmail.com>
2019-01-23 13:59:37 -0500

    began debugging of module, added main method for testing

--- subscraper.py
+++ subscraper.py
@@ -8,16 +8,17 @@ class Scraper:
 	def __init__(self,url):
 		self.url = url
 		self.headerend = 41
-		self.chromedriver = "./chromedriver"
-		self.driver = webdriver.Chrome(chromedriver)
-		self.actions = ActionChains(driver)
+		self.driver = webdriver.Chrome("./chromedriver")
+		self.actions = ActionChains(self.driver)
 		self.workerdict = {}
+		self.driver.get(self.url)
+		time.sleep(4)
 
-	def get_web_text():
-		return driver.find_element_by_tag_name("body").text.split("\n")[self.headerend:]
+	def get_web_text(self):
+		return self.driver.find_element_by_tag_name("body").text.split("\n")
 
-	def clean_web_text():
-		schedule = get_web_text()
+	def scrape(self):
+		schedule = self.get_web_text()[self.headerend:]
 		schedule.remove("ES Support Assistants")
 		schedule.remove("ES Student Operations")
 		for s in sorted(range(len(schedule)),reverse = True):
@@ -25,20 +26,34 @@ class Scraper:
 				del schedule[s]
 		workers = [schedule[x] for x in range(len(schedule)) if x % 2 ==0]
 		shifts = [schedule[x] for x in range(len(schedule)) if x % 2 !=0]
-		make_dict(workers,shifts)
+		self.make_dict(workers,shifts)
 
-	def make_dict(workers,shifts):
+	def make_dict(self,workers,shifts):
 		for x in range(len(workers)):
 			if workers[x] not in self.workerdict.keys():
 				self.workerdict[workers[x]] = [shifts[x]]
 			elif workers[x] in self.workerdict.keys() and shifts[x] not in self.workerdict[workers[x]]:
 				self.workerdict[workers[x]].append(shifts[x])
 
-	def print_dict():
-		for worker in workerdict.keys():
-			print(worker,workerdict[worker])
+	def print_dict(self):
+		for worker in self.workerdict.keys():
+			print(worker,self.workerdict[worker])
 			print('\n')
 
-	def close():
+	def close(self):
 		self.driver.close()
+
+'''
+Testing OOP
+'''
+def main():
+	scraper = Scraper("https://account.subitup.com/public/?5t7i0fpakJA%3d#byTimeDay")
+	scraper.scrape()
+	scraper.print_dict()
+	scraper.close()
+
+
+
+if __name__ == "__main__":
+	main()
 
